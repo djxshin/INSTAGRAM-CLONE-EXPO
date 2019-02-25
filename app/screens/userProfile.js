fcbf38ed56f9b09b4d9379df61e7da0123cbea9f
DJ Shin <djatlienriseup@gmail.com>
2019-02-25 16:47:41 -0500

    static userprofile setup

--- app/screens/userProfile.js
+++ app/screens/userProfile.js
@@ -11,11 +11,37 @@ class profile extends React.Component {
     }
 
 checkParams = () =>{
-
+    var params = this.props.navigation.state.params;
+    if(params){
+        if(params.userId){
+            this.setState({
+                userId: params.userId
+            });
+            this.fetchUserInfo(params.userId);
+        }
+    }
 }
 
 fetchUserInfo = (userId) => {
+    var that = this;
+    database.ref('users').child(userId).child('username').once('value').then(function(snapshot){
+        const exists = (snapshot.val() !==null);
+            if(exists) data = snapshot.val();
+                that.setState({username:data});
+    }).catch(error => console.log(error));
 
+    database.ref('users').child(userId).child('name').once('value').then(function(snapshot){
+        const exists = (snapshot.val() !==null);
+            if(exists) data = snapshot.val();
+                that.setState({name:data});
+    }).catch(error => console.log(error));
+
+        database.ref('users').child(userId).child('avatar').once('value').then(function(snapshot){
+        const exists = (snapshot.val() !==null);
+            if(exists) data = snapshot.val();
+                that.setState({avatar:data,
+                loaded:true});
+    }).catch(error => console.log(error));
 }
 
 componentDidMount = () =>{
@@ -30,14 +56,17 @@ componentDidMount = () =>{
                     </View>
                 ) : (
                     <View style={{flex:1}}>
-                    <View style={{height:70, paddingTop: 30, backgroundColor: 'white',  borderColor: 'lightgrey', borderBottomWidth: 0.5, justifyContent: 'center', alignItems: 'center'}}>
+                    <View style={{ flexDirection:"row", height:70, paddingTop: 30, backgroundColor: 'white',  borderColor: 'lightgrey', borderBottomWidth: 0.5, justifyContent: 'center', justifyConent:'space-between', alignItems: 'center'}}>
+                    <TouchableOpacity style={{width:100}} onPress={()=> this.props.navigation.goBack()} >
+                        <Text style={{fontSize:12, fontWeight:'bold', paddingLeft: 10}}>Go Back</Text>
+                    </TouchableOpacity>     
                          <Text>Profile</Text>
                     </View> 
                        <View style={{justifyContent: 'space-evenly', alignItems: 'center', flexDirection: 'row', paddingVertical: 10}}>
-                            <Image source={{ uri: 'https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png'}} style={{marginLeft: 10, width:100, height:100, borderRadius:50}}></Image>
+                            <Image source={{ uri: this.state.avatar }} style={{marginLeft: 10, width:100, height:100, borderRadius:50}}></Image>
                                 <View style={{marginRight: 10}}>
-                                    <Text>Name</Text>
-                                    <Text>@username</Text>
+                                    <Text>{this.state.name}</Text>
+                                    <Text>{this.state.username}</Text>
                                 </View>
                         </View>
                 
